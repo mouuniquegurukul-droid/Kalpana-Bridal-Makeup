b935e0579436944dd7a4f84c4aebad4d27376cd0
mouuniquegurukul-droid <mou.unique.gurukul@gmail.com>
2026-09-21 21:59:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Kalpana-Bridal-Makeup/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
